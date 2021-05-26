7450ed3ad01a57b79d7244fae3690372d7dfc7a9
Aslan <aslanstamaliev@gmail.com>
2021-05-26 16:21:19 +0600

    pervyi commit ))

---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,230 @@
+<template>
+  <div class="header-outer">
+    <section class="header-section">
+      <div class="container">
+        <header>
+          <div class="header-logo-nav">
+            <div class="logo">
+                <img src="../assets/images/logo.svg" alt="logo">
+            </div>
+            <!-- <div class="nav"> -->
+              <ul class="nav">
+                <li @mouseover="fallDownMenu('product')" 
+                    @mouseleave="productList = false"
+                    class="li-arrow">Product<span></span>
+                  <br>
+                  <div v-if="productList" class="product-content">
+                      <ul>
+                          <li>Overview</li>
+                          <li>Pricing</li>
+                          <li>Marketplace</li>
+                          <li>Features</li>
+                          <li>Integrations</li>
+                      </ul>
+                  </div>
+                </li>
+                <li @mouseover="fallDownMenu('company')"
+                    @mouseleave="companyList = false"
+                    class="li-arrow">Company<span></span>
+                  <br>
+                  <div v-if="companyList" class="product-content">
+                      <ul>
+                          <li>About</li>
+                          <li>Team</li>
+                          <li>Blog</li>
+                          <li>Careers</li>
+                      </ul>
+                  </div>
+                </li>
+                <li @mouseover="fallDownMenu('connect')"
+                    @mouseleave="connectList = false"
+                    class="li-arrow">Connect<span></span>
+                  <br>
+                  <div v-if="connectList" class="product-content">
+                      <ul>
+                          <li>Contact</li>
+                          <li>Newsletter</li>
+                          <li>LinkedIn</li>
+                      </ul>
+                  </div>
+                </li>
+              </ul>
+              <!-- <router-link to="/">Home</router-link> -->
+              <!-- <router-link to="/about">About</router-link> -->
+            <!-- </div> -->
+          </div>
+          <!-- <div class="reg-in"> -->
+            <ul class="reg-in">
+              <li class="login">Login</li>
+              <li class="sign-up">Sign Up</li>
+            </ul>
+          <!-- </div> -->
+        </header>
+
+        <div class="head-content">
+          <h1>A modern publishing platform</h1>
+          <h4>Grow your audience and build your online brand</h4>
+          <ul>
+            <a href="#" class="freeStart"><li>Start for free</li></a>
+            <a href="#" class="learnMore"><li>Learn More</li></a>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderSection',
+    components: {
+
+    },
+    data() {
+      return {
+        productList: false,
+        companyList: false,
+        connectList: false,
+
+        
+      }
+    },
+    methods: {
+      fallDownMenu(el) {
+        if(el === 'product') {
+          this.productList = true
+        } else if (el === 'company') {
+          this.companyList = true
+        } else if (el === 'connect') {
+          this.connectList = true
+        }
+      }
+    }
+
+}
+</script>
+
+<style lang="scss" >
+  .header-outer {
+    height: 100vh;
+  }
+    section.header-section {
+        height: 85vh;
+        color: $white;
+        background: rgb(255,143,112);
+        background-image: url("../assets/images/bg-pattern-intro.svg");
+        background-repeat: no-repeat;
+        background-size: auto auto;
+        background-position: 40% 50%;
+        border-bottom-left-radius: 70px;
+        
+
+
+        header {
+          display: flex;
+          justify-content: space-between;
+          height: 150px;
+
+          .reg-in {
+            float: right;
+            
+            .login {
+              @include headBtns($bgColorIs);
+              font-family: $btnFont;
+            }
+
+            .sign-up {
+              background-color: $white;
+              color: $lightRed;
+              transition-property: background-color;
+              font-family: $btnFont;
+              @include headBtns($white);
+            }
+            
+          }
+        }
+
+        .header-logo-nav {
+            display: flex;
+            align-items: center;
+
+
+
+            li.li-arrow {
+                padding: 8px;
+                margin-left: 45px;
+                position: relative;
+
+
+                ul {
+                    display: block;
+                    position: absolute;
+                    color: $veryDarkBlue;
+                    background-color: $white;
+                    padding: 25px 25px;
+                    border-radius: 8px;
+                    line-height: 2;
+                    z-index: 2;
+                    margin-left: -20px;
+                    margin-top: 7px;
+
+                    li {
+                        margin: 0;
+                        font-weight: 300;
+                    }
+                    li:hover {
+                      font-weight: 600;
+                    }
+                }
+                
+                span {
+                    display: inline-block;
+                    width: 10px;
+                    height: 7px;
+                    background-image: url("../assets/images/icon-arrow-light.svg");
+                    background-repeat: no-repeat;
+                    transition: all 0.4s ease;
+                    margin-left: 5px;
+                }
+
+            }
+            .li-arrow:hover {
+                text-decoration: underline;
+                span {
+                    transform: rotate(180deg);
+                }
+            }
+        }
+
+      .head-content {
+          text-align: center;
+
+        h1 {
+          font-size: 4rem;
+          margin-top: .7em;
+          margin-bottom: 0.2em;
+        }
+        h4 {
+          font-size: 1.5rem;
+          font-weight: 300;
+          margin-bottom: 1.7em;
+        }
+        ul {
+          display: flex;
+          justify-content: center;
+        }
+        .freeStart {
+          background-color: $white;
+          color: $veryLightRed;
+          margin-right: 30px;
+          @include headBtns($white);
+        }
+        .learnMore {
+          // transition-duration: 1s;
+          color: $white;
+          border: 1px solid $white;
+          @include headBtns($veryLightRed);
+        }
+      }
+    }
+</style>
---
+++ src/components/IntroSection.vue
@@ -0,0 +1,147 @@
+<template>
+    <section class="intro-sec">
+            <div class="container">
+                <h2 class="fade-in" id="test">Designed for the future</h2>
+
+                <div class="intro-inner">
+                        <div class="intro-inner-block fade-in">
+                            <h3>Introducing an extensible editor</h3>
+                            <p>Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. 
+                            The editor suppurts management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. 
+                            Extensibility with plugins and themes provide easy ways to add functionality or change the looks of a blog.</p>
+                            
+                            <h3>Robust content management</h3>
+                            <p>Flexible content management enable users to easily move through posts. 
+                            Increase the usability of your blog by adding  customized categories, sections, format, or flow. 
+                            With this functionality, you're in full control.</p>
+                        </div>
+                        <!-- <div class="intro-inner-image">
+                            <img class="" src="../assets/images/illustration-editor-desktop.svg">
+                        </div> -->
+                </div>
+            </div>
+    </section>
+    <section class="art-infra">
+        <div class="art-infra-bg-block">
+            <div class="container">
+                <div class="art-infra-inner-block">
+                    <div class="art-img fade-in">
+                        <img src="../assets/images/illustration-phones.svg" alt="">
+                    </div>
+                    <div class="art-text fade-in">
+                        <h3>State of the Art Infrastructure</h3>
+                        <p>With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity.
+                        This ensures your site will load instantly, no matter where your reader are, keeping your site competitive.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+
+export default {
+    name: "IntroSection",
+    data() {
+        return {
+            fadeInElements : []
+        }
+    },
+
+    mounted() {
+        this.fadeInElements = Array.from(document.getElementsByClassName('fade-in'))
+        document.addEventListener('scroll', this.handleScroll)
+        this.handleScroll()
+    },
+
+    methods: {
+        handleScroll() {
+            for(var i = 0; i < this.fadeInElements.length; i++) {
+                var elem = this.fadeInElements[i]
+                if(this.isElemVisible(elem)) {
+                    elem.style.opacity = '1'
+                    elem.style.transform ='scale(1)'
+                    this.fadeInElements.splice(i, 1)
+                }
+
+            }
+        },
+        isElemVisible(el) {
+            var rect = el.getBoundingClientRect()
+            var elemTop = rect.top + 200
+            var elemBottom = rect.bottom
+            return elemTop < window.innerHeight && elemBottom >=0
+        }
+    }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    
+    .intro-sec {
+        height: 100vh;
+        position: relative;
+
+        h2 {
+            text-align: center;
+        }
+
+        .intro-inner {
+                        
+            .intro-inner-block{
+                width: 50%;
+                margin-top: 3.3em;
+                padding-right: 1.2em;
+            }
+        }
+    }
+    .intro-sec::after {
+        content: "";
+        position: absolute;
+        float: right;
+        top: -110px;
+        right: -280px;
+        width: 60%;
+        height: 120%;
+        background-image: url(../assets/images/illustration-editor-desktop.svg);
+        background-size: cover;
+        background-repeat: no-repeat;
+        
+    }
+
+    // State of the Art Infrastructure
+
+    .art-infra {
+        height: 100vh;
+        display: flex;
+
+        &-bg-block {
+            height: 400px;
+            background-color: $veryDarkGrayBlue;
+            border-bottom-left-radius: 70px;
+            border-top-right-radius: 70px;
+            align-self: center;
+            width: 100%;
+            background-image: url('../assets/images/bg-pattern-circles.svg');
+            background-repeat: no-repeat;
+            background-position: -40% 80%;
+
+        }
+        &-inner-block {
+            display: flex;
+            .art-img {
+                margin-top: -70px
+            }
+            .art-text > *{
+                color: $white;
+            }
+            .art-text {
+                margin-top: 75px;
+                padding-left: 30px;
+            }
+        }
+    }
+</style>
